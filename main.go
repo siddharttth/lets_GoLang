18ceb7cc6362570c7568a1566f2824e377c9919a
SIDDHARTH SHEKHAR <75613480+siddharttth@users.noreply.github.com>
2024-02-12 01:13:04 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("error : ", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, f)
+}
